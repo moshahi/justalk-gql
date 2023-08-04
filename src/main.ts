149869c2b2651f0c5bf21901935b64584deec379
Mohammad Shahi <mohammad.mft.shahi@gmail.com>
2023-08-04 13:51:09 +0330

    add cors to project

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,10 @@ import * as cookieParser from 'cookie-parser';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(cookieParser());
+  app.enableCors({
+    origin: '*',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  });
   await app.listen(3000);
 }
 bootstrap();
